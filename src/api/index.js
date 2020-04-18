03690d1a4e1572a295f6db0851e38fb0930e16bf
jordan <jordan@jordans-MacBook-Air.local>
2020-04-18 14:06:37 -0700

    dependencies

---
+++ src/api/index.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+
+
+
+
+
+const key = 'unpFCmtnqlhuQSKfZyRNA3T4Md9zOQ2jU8o3mNag';
+
+
+
+const url = `https://api.usa.gov/crime/fbi/sapi/api/data/nibrs/${offense}/offense/states/${stateAbbr}/${variable}?API_KEY=${key}`;
