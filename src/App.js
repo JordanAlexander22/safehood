c39b5a936081f2d61ce3d1cf908ca53758cac828
jordan <jordan@jordans-MacBook-Air.local>
2020-04-18 13:46:53 -0700

    test

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class App extends React.Component {
+  state = {}
+
+    render() {
+      return (
+        <div className="Container">
+          <h1>Test</h1>
+        </div>
+      );
+    }
+  };
+
+export default App;
